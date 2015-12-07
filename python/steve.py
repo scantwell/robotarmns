2b2d9bca31376953cbbf4217e0bd20edc2804cef
Matt Higgins <xm1337@gmailcom>
2015-12-07 14:48:45 -0500

    message

--- python/steve.py
+++ python/steve.py
@@ -1,64 +1,52 @@
 #!/usr/bin/python2.7
 
-import pygame
 import serial
 import struct
 
-tty = "/dev/ttyUSB0"
+tty = "/dev/tty.usbserial-A603GDO7"
 robot = serial.Serial(tty, 9600, timeout=5)
+robot.close()
 robot.open()
 
-pygame.init()
-pygame.key.set_repeat(100, 100)
 
 # COMMANDS
-PINCER = '0'
-ARM = '1'
-FORWARD = '2' 
-BACKWARD = '3'
-TURN_LEFT = '4'
-TURN_RIGHT = '5'
-
-UP = "u" #273
-DOWN = "d" #274
-RIGHT = "r" #275
-LEFT = "l" #276
+UP = "up"
+DOWN = "down"
+RIGHT = "right"
+LEFT = "left"
+OPEN = "open"
+CLOSE = "close"
+RAISE = "raise"
+LOWER = "lower"
 
 def run():
-
     while True:
-#        events = pygame.event.get()
-#        for event in events:
-        input = raw_input() 
-        print input
+        input = raw_input()
         if input == UP:
             robot.write(struct.pack('I', 2))
             robot.write(struct.pack('I', 2500))
-            print 'KEY UP'
         elif input == DOWN:
-            print 'KEY DOWN'
             robot.write(struct.pack('I', 3))
             robot.write(struct.pack('I', 2500))
         elif input == RIGHT:
-            print 'KEY RIGHT'
             robot.write(struct.pack('I', 5))
             robot.write(struct.pack('I', 1000))
         elif input == LEFT:
-            print 'KEY LEFT'
             robot.write(struct.pack('I', 4))
             robot.write(struct.pack('I', 1000))
-        elif input == "open":
+        elif input == OPEN:
             robot.write(struct.pack('I', 0))
             robot.write(struct.pack('I', 0))
-        elif input == "close":
+        elif input == CLOSE:
             robot.write(struct.pack('I', 0))
             robot.write(struct.pack('I', 100))
-        elif input == "up":
+        elif input == RAISE:
             robot.write(struct.pack('I', 1))
             robot.write(struct.pack('I', 5))
-        elif input == "down":
+        elif input == LOWER:
             robot.write(struct.pack('I', 1))
             robot.write(struct.pack('I', 100))
         print robot.read()
+
 if __name__ == '__main__':
     run()
--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,6 @@
 var express = require('express');
-var child_process = require('child_process');
-
+var spawn = require('child_process').spawn;
 var router = express.Router();
-//var serialpy = child_process.exec('serial.py');
 
 router.get('/', function(req, res) {
   // API help page or something?
@@ -10,47 +8,95 @@ router.get('/', function(req, res) {
 });
 
 router.get('/ping', function(req, res)  {
-  /*
-  var serialpy = child_process.exec('serial.py');
-  serialpy.stdin.write('ping');
-  serialpy.stdout.on('data', function(data) {
-    serialpy.kill();
-    res.send(data);
-  });
-  */
   res.send('success');
 });
 
 router.post('/moveforward', function(req, res) {
-  res.send('success');
+  var py = spawn('python', ['/Users/omnia/WebstormProjects/robotarmns/python/robotarm.py', 'forward', '2500']);
+  py.on('close', function(exitCode) {
+    if (exitCode == 0) {
+      res.send('success')
+    } else {
+      res.status(500).send('failure')
+    }
+  });
 });
 
 router.post('/movebackward', function(req, res) {
-  res.send('success');
+  var py = spawn('python', ['/Users/omnia/WebstormProjects/robotarmns/python/robotarm.py', 'backward', '2500']);
+  py.on('close', function(exitCode) {
+    if (exitCode == 0) {
+      res.send('success')
+    } else {
+      res.status(500).send('failure')
+    }
+  });
 });
 
 router.post('/turnleft', function(req, res) {
-  res.send('success');
+  var py = spawn('python', ['/Users/omnia/WebstormProjects/robotarmns/python/robotarm.py', 'left', '1000']);
+  py.on('close', function(exitCode) {
+    if (exitCode == 0) {
+      res.send('success')
+    } else {
+      res.status(500).send('failure')
+    }
+  });
 });
 
 router.post('/turnright', function(req, res) {
-  res.send('success');
+  var py = spawn('python', ['/Users/omnia/WebstormProjects/robotarmns/python/robotarm.py', 'right', '1000']);
+  py.on('close', function(exitCode) {
+    if (exitCode == 0) {
+      res.send('success')
+    } else {
+      res.status(500).send('failure')
+    }
+  });
 });
 
 router.post('/openclaw', function(req, res) {
-  res.send('success');
+  var py = spawn('python', ['/Users/omnia/WebstormProjects/robotarmns/python/robotarm.py', 'claw', '5']);
+  py.on('close', function(exitCode) {
+    if (exitCode == 0) {
+      res.send('success')
+    } else {
+      res.status(500).send('failure')
+    }
+  });
 });
 
 router.post('/closeclaw', function(req, res) {
-  res.status(500).send('failure');
+  var py = spawn('python', ['/Users/omnia/WebstormProjects/robotarmns/python/robotarm.py', 'claw', '95']);
+  py.on('close', function(exitCode) {
+    if (exitCode == 0) {
+      res.send('success')
+    } else {
+      res.status(500).send('failure')
+    }
+  });
 });
 
 router.post('/raisearm', function(req, res) {
-  res.send('success');
+  var py = spawn('python', ['/Users/omnia/WebstormProjects/robotarmns/python/robotarm.py', 'arm', '5']);
+  py.on('close', function(exitCode) {
+    if (exitCode == 0) {
+      res.send('success')
+    } else {
+      res.status(500).send('failure')
+    }
+  });
 });
 
 router.post('/lowerarm', function(req, res) {
-  res.send('success');
+  var py = spawn('python', ['/Users/omnia/WebstormProjects/robotarmns/python/robotarm.py', 'arm', '95']);
+  py.on('close', function(exitCode) {
+    if (exitCode == 0) {
+      res.send('success')
+    } else {
+      res.status(500).send('failure')
+    }
+  });
 });
 
 module.exports = router;
